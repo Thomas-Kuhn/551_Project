0ee272c3c793519b3463683030d8b752ac6796b0
Thomas-Kuhn <tkuhn@WIN-8038.ad.engr.wisc.edu>
2015-12-06 13:11:38 -0600

    Updated LP_fir.v
    Added basic state machine.

--- LP_fir.v
+++ LP_fir.v
@@ -7,7 +7,7 @@ output [15:0] filtered_L, filtered_R;
 reg [9:0] addr;
 wire signed [15:0] coef;
 reg signed [32:0] ans_L, ans_R;
-reg state;
+reg state, next_state;
 
 ROM_LP(.clk(clk), .addr(addr), .dout(coef));
 
@@ -17,20 +17,35 @@ assign filtered_R = ans_R[30:15];
 
 always @ (posedge clk) begin
 
-   if (sequencing ) begin
-      if (state == 0) begin 
-         state <= 1;
-         addr <= 0;
-         ans_L <= 0;
-         ans_R <= 0;
-      end else begin
-         ans_L <= ans_L + coef * lft_in;
-         ans_R <= ans_R + coef * rht_in;
-         addr = (addr + 1) % 10'h3fd;
-      end
-   end else state = 0;
+
+   if (state == 0) begin 
+       
+      addr <= 0;
+      ans_L <= 0;
+      ans_R <= 0;
+
+   end else begin
+      ans_L <= ans_L + coef * lft_in;
+      ans_R <= ans_R + coef * rht_in;
+      addr = (addr + 1) % 10'h3fd;
+   end
+
    
 
+end
+
+always @(*) begin
+
+   next_state = 0;
+
+   if (sequencing) begin
+      next_state = 1;
+   end 
+
+end
+
+always @(posedge clk) begin
+   state <= next_state;
 end
 
 
